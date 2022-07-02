ec703eb3ebace058aed10c43e950900593846f0f
Etnan Sousa <poketnans@gmail.com>
2022-07-02 02:10:02 -0300

    feat: adds address fields

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -1,9 +1,11 @@
+from hashlib import blake2s
 from uuid import uuid4
 
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
-from django.forms import CharField
+
+from accounts.utils import STATES_CHOICES
 
 
 class AccountModel(AbstractUser):
@@ -17,14 +19,22 @@ class AccountModel(AbstractUser):
         db_table = "accounts"
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
+    first_name = models.CharField(max_length=255, null=False, blank=False)
     last_name = models.CharField(max_length=255)
     zip_code = models.CharField(
         max_length=8,
         validators=[RegexValidator(_zip_code_pattern, _zip_code_error_msg)],
         null=False,
+        blank=False,
     )
-    address = models.CharField(max_length=255, null=False)
+    street = models.CharField(max_length=255, null=False, blank=False)
+    city = models.CharField(max_length=255, null=False, blank=False)
+    state = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        choices=STATES_CHOICES,
+    )
     created_at = models.DateTimeField(editable=False, auto_now_add=True)
     updated_at = models.DateTimeField(editable=False, auto_now=True)
