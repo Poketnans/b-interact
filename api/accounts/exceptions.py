39b19eadfd9f7a3f8f88105f45fcf5612d07c85c
Etnan Sousa <poketnans@gmail.com>
2022-07-05 00:05:03 -0300

    refactor: ajusts zip code error format

--- api/accounts/exceptions.py
+++ api/accounts/exceptions.py
@@ -4,4 +4,4 @@ from rest_framework.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 class InvalidZipCodeError(APIException):
     status_code = HTTP_422_UNPROCESSABLE_ENTITY
-    default_detail = {"error": "Invalid zip code."}
+    default_detail = {"zip_code": ["Invalid zip code."]}
