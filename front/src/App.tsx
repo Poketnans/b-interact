d047165d9d819a5659cf4c3815bb586896914720
Etnan Sousa <poketnans@gmail.com>
2022-07-06 20:40:45 -0300

    chore: eliminates unused elements

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,16 @@
 import * as React from "react";
-import { ChakraProvider, Box, VStack, Grid, theme } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { VStack, Grid } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./components/ColorModeSwitcher/ColorModeSwitcher";
 import { RegisterUser } from "./components/RegisterForm";
 import { AppProvider } from "./providers";
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <AppProvider>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <RegisterUser />
-          </VStack>
-        </Grid>
-      </Box>
-    </AppProvider>
-  </ChakraProvider>
+  <AppProvider>
+    <Grid minH="100vh" p={3}>
+      <ColorModeSwitcher justifySelf="flex-end" />
+      <VStack spacing={8}>
+        <RegisterUser />
+      </VStack>
+    </Grid>
+  </AppProvider>
 );
