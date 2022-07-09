a931602b535d07d475329737bc16d4c3b6a66fe5
Etnan Sousa <poketnans@gmail.com>
2022-07-08 23:25:40 -0300

    feat: implements custom exceptions

--- api/accounts/exceptions.py
+++ api/accounts/exceptions.py
@@ -1,7 +1,17 @@
 from rest_framework.exceptions import APIException
-from rest_framework.status import HTTP_422_UNPROCESSABLE_ENTITY
+from rest_framework.status import HTTP_409_CONFLICT, HTTP_422_UNPROCESSABLE_ENTITY
 
 
 class InvalidZipCodeError(APIException):
     status_code = HTTP_422_UNPROCESSABLE_ENTITY
     default_detail = {"zip_code": ["Invalid zip code."]}
+
+
+class AlreadyRegisteredUsernameError(APIException):
+    status_code = HTTP_409_CONFLICT
+    default_detail = {"username": ["A user with that username already exists."]}
+
+
+class AlreadyRegisteredEmailError(APIException):
+    status_code = HTTP_409_CONFLICT
+    default_detail = {"email": ["A user with this email already exists."]}
--- api/accounts/views.py
+++ api/accounts/views.py
@@ -1,5 +1,9 @@
 from rest_framework.generics import CreateAPIView
 
+from accounts.exceptions import (
+    AlreadyRegisteredEmailError,
+    AlreadyRegisteredUsernameError,
+)
 from accounts.models import AccountModel
 from accounts.serializers import AccountSerializer
 
@@ -7,3 +11,25 @@ from accounts.serializers import AccountSerializer
 class CreateAccountView(CreateAPIView):
     serializer_class = AccountSerializer
     queryset = AccountModel.objects
+
+    def create(self, request, *args, **kwargs):
+        email = (
+            self.get_queryset().filter(email__iexact=request.data["email"]).exists()
+            if request.data.get("email")
+            else None
+        )
+
+        if email:
+            raise AlreadyRegisteredEmailError()
+
+        username = (
+            self.get_queryset()
+            .filter(username__iexact=request.data["username"])
+            .exists()
+            if request.data.get("username")
+            else None
+        )
+
+        if username:
+            raise AlreadyRegisteredUsernameError()
+        return super().create(request, *args, **kwargs)
